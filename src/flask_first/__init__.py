3fe2866df163fd3b68590a1712d120913317c798
Konstantin Fadeev <fadeev@stantis.ru>
2021-12-01 17:38:39 +0300

    add SwaggerUI.

--- src/flask_first/__init__.py
+++ src/flask_first/__init__.py
@@ -23,7 +23,7 @@ from .spec_parser import Specification
 from .validators import validate_json
 from .validators import validate_params
 
-__version__ = '0.6.0.dev'
+__version__ = '0.6.0'
 
 
 class First:

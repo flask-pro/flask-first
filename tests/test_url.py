25f13197900ffd3eaedf7006da92e6d8d7052ea3
Konstantin Fadeev <fadeev@stantis.ru>
2023-09-04 20:37:00 +0300

    fix case-sensitive in url.

--- tests/test_url.py
+++ tests/test_url.py
@@ -7,30 +7,30 @@ from .conftest import BASEDIR
 
 
 def test_specification__case_sensitive_url(fx_create_app):
-    def non_case_sensitive_url_endpoint() -> dict:
+    def case_sensitive_url_endpoint() -> dict:
         return {'message': request.url_rule.rule}
 
     test_client = fx_create_app(
-        Path(BASEDIR, 'specs/v3.0/url.non_case_sensitive.openapi.yaml'),
-        (non_case_sensitive_url_endpoint,),
+        Path(BASEDIR, 'specs/v3.0/url.case_sensitive.openapi.yaml'),
+        (case_sensitive_url_endpoint,),
     )
 
-    url = '/NonCaseSensitiveURL'
+    url = '/CaseSensitiveURL'
     r = test_client.get(url)
     assert r.status_code == 200
     assert r.json['message'] == url
 
 
 @pytest.mark.parametrize(
-    'url', ('/noncasesensitiveurl', '/Noncasesensitiveurl', '/NONCASESENSITIVEURL')
+    'url', ('/casesensitiveurl', '/casesensitiveurl', '/CASESENSITIVEURL', '/CASE-SENSITIVE_URL')
 )
 def test_specification__non_case_sensitive_url(fx_create_app, url):
-    def non_case_sensitive_url_endpoint() -> dict:
-        return {'message': request.url}
+    def case_sensitive_url_endpoint() -> dict:
+        return {'message': request.url_rule.rule}
 
     test_client = fx_create_app(
-        Path(BASEDIR, 'specs/v3.0/url.non_case_sensitive.openapi.yaml'),
-        (non_case_sensitive_url_endpoint,),
+        Path(BASEDIR, 'specs/v3.0/url.case_sensitive.openapi.yaml'),
+        (case_sensitive_url_endpoint,),
     )
 
     r = test_client.get(url)

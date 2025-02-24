741e3e0352f22d31fe6134411f00ba794f68deef
Konstantin Fadeev <fadeev@stantis.ru>
2025-02-24 17:57:42 +0300

    add settings for define format string input datetime format.

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from datetime import UTC
 from pathlib import Path
 
 import pytest
@@ -32,7 +31,7 @@ def test_responses__datetime(fx_create_app):
         Path(BASEDIR, 'specs/v3.1.0/datetime.openapi.yaml'), [create_datetime]
     )
 
-    json = {'datetime': datetime.now(UTC).strftime("%Y-%m-%dT%H:%M:%S.%fZ")}
+    json = {'datetime': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")}
     r = test_client.post('/datetime', json=json)
     assert r.status_code == 200
     assert r.json['datetime'] == json['datetime']
@@ -47,7 +46,7 @@ def test_responses__request_datetime_error(fx_create_app):
         Path(BASEDIR, 'specs/v3.1.0/datetime.openapi.yaml'), [create_datetime]
     )
 
-    json = {'datetime': datetime.now(UTC).isoformat()}
+    json = {'datetime': datetime.utcnow().isoformat()}
 
     with pytest.raises(FirstRequestJSONValidation):
         test_client.post('/datetime', json=json)
@@ -62,7 +61,7 @@ def test_responses__response_datetime_error(fx_create_app):
         Path(BASEDIR, 'specs/v3.1.0/datetime.openapi.yaml'), [create_datetime]
     )
 
-    json = {'datetime': datetime.now(UTC).strftime("%Y-%m-%dT%H:%M:%S.%fZ")}
+    json = {'datetime': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")}
 
     with pytest.raises(FirstResponseJSONValidation):
         test_client.post('/datetime', json=json)

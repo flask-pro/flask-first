0871c1784b6822d2f6cb31ea09cce3f103189fc9
Konstantin Fadeev <fadeev@stantis.ru>
2025-03-04 15:43:12 +0300

    fix ref resolver.

--- tests/first/loaders/yaml_loader/conftest.py
+++ tests/first/loaders/yaml_loader/conftest.py
@@ -1,7 +1,10 @@
+from collections.abc import Iterable
 from pathlib import Path
 
 import pytest
 import yaml
+from flask import Flask
+from flask_first import First
 from openapi_spec_validator import validate as osv_validate
 from openapi_spec_validator.readers import read_from_filename
 
@@ -49,3 +52,24 @@ def fx_spec_as_file(tmp_path):
         return spec_path
 
     return create
+
+
+@pytest.fixture()
+def fx_create_app():
+    def _create_app(path_to_spec: str, routes_functions: Iterable):
+        app = Flask('testing_app')
+        app.debug = True
+        app.config['FIRST_RESPONSE_VALIDATION'] = True
+        app.config['FIRST_DATETIME_FORMAT'] = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+        first = First(path_to_spec, app, swagger_ui_path='/docs')
+        for func in routes_functions:
+            first.add_view_func(func)
+
+        app_context = app.app_context()
+        app_context.push()
+
+        with app.test_client() as test_client:
+            return test_client
+
+    return _create_app
--- tests/first/loaders/yaml_loader/test_loaders__yaml_loader__multiple_files.py
+++ tests/first/loaders/yaml_loader/test_loaders__yaml_loader__multiple_files.py
@@ -6,6 +6,10 @@ from src.flask_first.first.loaders.yaml_loader import load_from_yaml
 
 def test_loaders__yaml__multiple__response_ref(fx_spec_minimal, fx_spec_as_file):
     endpoint_spec = {'endpoint': fx_spec_minimal['paths']['/endpoint']}
+    endpoint_spec['endpoint']['get']['responses']['200']['content']['application/json']['schema'][
+        'properties'
+    ]['message'] = {'$ref': '#/components/schemas/Message'}
+    endpoint_spec['components'] = {'schemas': {'Message': {'type': 'string'}}}
     endpoint_spec_file = fx_spec_as_file(endpoint_spec, validate=False, file_name='endpoint.yaml')
 
     fx_spec_minimal['paths']['/endpoint'] = {'$ref': f'{endpoint_spec_file.name}#/endpoint'}
@@ -13,7 +17,9 @@ def test_loaders__yaml__multiple__response_ref(fx_spec_minimal, fx_spec_as_file)
     spec_obj = load_from_yaml(spec_file)
 
     assert spec_obj['paths']['/endpoint'].get('$ref') is None
-    assert spec_obj['paths']['/endpoint'] == endpoint_spec['endpoint']
+    assert spec_obj['paths']['/endpoint']['get']['responses']['200']['content']['application/json'][
+        'schema'
+    ]['properties']['message'] == {'type': 'string'}
 
 
 def test_loader__internal__multiple__non_exist_file(fx_spec_minimal, fx_spec_as_file):
@@ -25,3 +31,28 @@ def test_loader__internal__multiple__non_exist_file(fx_spec_minimal, fx_spec_as_
         load_from_yaml(spec_file)
 
     assert str(e.value) == f'No such file or directory: <{non_exist_file_name}>'
+
+
+def test_loaders__yaml__multiple__method_as_ref(fx_spec_minimal, fx_spec_as_file, fx_create_app):
+    method_spec = {'CORS': {'summary': 'CORS support'}}
+    method_spec_file = fx_spec_as_file(method_spec, validate=False, file_name='CORS.openapi.yaml')
+
+    endpoint_spec = {'endpoint': fx_spec_minimal['paths']['/endpoint']}
+    endpoint_spec['endpoint']['options'] = {'$ref': f'{method_spec_file.name}#/CORS'}
+    endpoint_spec_file = fx_spec_as_file(endpoint_spec, validate=False, file_name='endpoint.yaml')
+
+    fx_spec_minimal['paths']['/endpoint'] = {'$ref': f'{endpoint_spec_file.name}#/endpoint'}
+    spec_file = fx_spec_as_file(fx_spec_minimal, validate=False)
+    spec_obj = load_from_yaml(spec_file)
+
+    assert spec_obj['paths']['/endpoint'].get('$ref') is None
+    assert spec_obj['paths']['/endpoint']['get'] == endpoint_spec['endpoint']['get']
+    assert spec_obj['paths']['/endpoint']['options'] == method_spec['CORS']
+
+    def endpoint() -> dict:
+        return {'message': 'OK'}
+
+    test_client = fx_create_app(spec_file, [endpoint])
+    message = test_client.get('/endpoint')
+
+    assert message.json == {'message': 'OK'}
